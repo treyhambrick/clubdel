48d618fadd9b3048dce04f0abc2ed9114b7b7e78
treyhambrick <trey.hambrick@gmail.com>
2023-09-25 17:57:23 -0400

    Initial dist subtree commit

--- run.bat
+++
@@ -1,8 +0,0 @@
-git init
-git add .
-git commit -m "first commit1234"
-git branch -M main
-git remote add origin https://github.com/treyhambrick/clubdel.git
-git push -u origin main
-git add dist && git commit -m "Initial dist subtree commit"
-git subtree push --prefix dist origin gh-pages
