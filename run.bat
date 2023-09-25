c13d35a6d609975fba5cafd0da418045bf0e5ff7
treyhambrick <trey.hambrick@gmail.com>
2023-09-25 17:50:15 -0400

    first commit123

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ git init
 git add .
 git commit -m "first commit1234"
 git branch -M main
-git remote add origin https://github.com/treyhambrick/club.git
+git remote add origin https://github.com/treyhambrick/clubdel.git
 git push -u origin main
 git add dist && git commit -m "Initial dist subtree commit"
 git subtree push --prefix dist origin gh-pages
